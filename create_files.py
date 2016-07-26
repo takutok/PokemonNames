59e8dd8a26c407d0c8952496a711adc2d1dd314d
takutok <tkhearts@gmail.com>
2016-07-26 14:25:00 +0900

    modified create_files.py add multilang pokemon names string files.

--- create_files.py
+++ create_files.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python
 # coding: utf-8
 import json
+import os
 
-f = open('pokemon.ja.json', 'r')
-fen = open('pokemon.en.json', 'r')
+lang_list = ["de","fr","ja","pt_br","ru","zh_cn","zh_hk"]
+rf_en = open('PokemonGo-Map/static/locales/pokemon.en.json', 'r')
+json_data_en = json.load(rf_en)
+  
+for lang in lang_list:
+  print "Encode : %s" % lang
+  rf = open('PokemonGo-Map/static/locales/pokemon.%s.json' % lang, 'r')
+  if not os.path.exists("res/values-%s" % lang):
+    os.makedirs("res/values-%s" % lang)
+  wf = open('res/values-%s/pokemon.xml' % lang, 'w')
+  json_data_l = json.load(rf)
+  wf.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n");
 
-json_data_l = json.load(f)
-json_data_en = json.load(fen)
+  for n in range(1, len(json_data_l)):
+    if str(n) in json_data_l:
+      wf.write("\t<string name=\"{0}\">{1}</string>\n".format(json_data_en[str(n)].lower().replace(".","").replace(" ", "_").encode('utf-8'), json_data_l[str(n)].encode('utf-8')));
 
-print "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>"
+  wf.write("</resources>");
+  rf.close()
+  wf.close()
+
+#Defalut
+if not os.path.exists("res/values"):
+  os.makedirs("res/values")
+wf = open('res/values/pokemon.xml', 'w')
+wf.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n");
 
 for n in range(1, len(json_data_en)):
-  print "\t<string name=\"{0}\">{1}</string>".format(json_data_en[str(n)].encode('utf-8'), json_data_l[str(n)].encode('utf-8'));
+  wf.write("\t<string name=\"{0}\">{1}</string>\n".format(json_data_en[str(n)].lower().replace(".","").replace(" ", "_").encode('utf-8'), json_data_en[str(n)].encode('utf-8')));
 
-print "</resources>"
+wf.write("</resources>");
 
-f.close()
+rf_en.close()
+wf.close()
+
